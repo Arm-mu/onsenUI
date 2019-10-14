758e4916099cf14cd7538f5a636cd5dcc07f9ab1
FoxLyz <38246160+FoxLyz@users.noreply.github.com>
2019-10-15 01:24:31 +0700

    Deployed SignIn and SignUp <3
    Deployed SignIn and SignUp. also CHANGE all 'login' to 'signin' same with signup and signout

--- js/app.js
+++ js/app.js
@@ -53,13 +53,13 @@ document.addEventListener('init', function (event) {
 
     if (page.id === "sidemenu") {
         //Code for sidemenu
-        $("#loginsigninbtn").click(function () {
+        $("#signinsignupbtn").click(function () {
             var content = document.getElementById('content');
             var menu = document.getElementById('menu');
             content.load('tabbar.html')
                 .then(menu.close.bind(menu));
         });
-        $("#logoutbtn").click(function () {
+        $("#signoutbtn").click(function () {
             firebase.auth().signOut().then(function () {
                 // Sign-out successful.
                 $("#content")[0].load("tabbar.html");
@@ -117,6 +117,71 @@ document.addEventListener('init', function (event) {
                 // ...
             });
         });
+        $("#toggleSignIn").click(function () {
+            if (firebase.auth().currentUser) {
+                // [START signout]
+                firebase.auth().signOut();
+                // [END signout]
+            } else {
+                var email = document.getElementById('email').value;
+                var password = document.getElementById('password').value;
+                if (email.length < 4) {
+                    alert('Please enter an email address.');
+                    return;
+                }
+                if (password.length < 4) {
+                    alert('Please enter a password.');
+                    return;
+                }
+                // Sign in with email and pass.
+                // [START authwithemail]
+                firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
+                    // Handle Errors here.
+                    var errorCode = error.code;
+                    var errorMessage = error.message;
+                    // [START_EXCLUDE]
+                    if (errorCode === 'auth/wrong-password') {
+                        alert('Wrong password.');
+                    } else {
+                        alert(errorMessage);
+                    }
+                    console.log(error);
+                    document.getElementById('quickstart-sign-in').disabled = false;
+                    // [END_EXCLUDE]
+                });
+                // [END authwithemail]
+            }
+            document.getElementById('quickstart-sign-in').disabled = true;
+        });
     }
 
+    if (page.id == 'tab2') {
+        $('#toggleSignUp').click(function () {
+            var email = document.getElementById('emailsignup').value;
+            var password = document.getElementById('passwordsignup').value;
+            //var fullname = document.getElementById('fullnamesignup').value;
+            //var phone = document.getElementById('phonenumbersignup').value;
+            if (email.length < 4) {
+                alert('Please enter an email address.');
+                return;
+            }
+            if (password.length < 4) {
+                alert('Please enter a password.');
+                return;
+            }
+            firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
+                // Handle Errors here.
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                // [START_EXCLUDE]
+                if (errorCode == 'auth/weak-password') {
+                    alert('The password is too weak.');
+                } else {
+                    alert(errorMessage);
+                }
+                console.log(error);
+                // [END_EXCLUDE]
+            });
+        });
+    }
 });
